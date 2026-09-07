ba7871adb29b635ca4c01a672aa9745ec3b50a9c
User <user@example.com>
2026-09-07 08:02:17 +0100

    fix: remove accidentally committed validator file

--- tmp_validate.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs');
-const html=fs.readFileSync('customer-history.html','utf8');
-const scripts=html.match(/<script>([\s\S]*?)<\/script>/g)||[];
-scripts.forEach((s,i)=>{
-  const code=s.replace(/^<script>|<\/script>$/g,'');
-  try{new Function(code);console.log('Script '+i+': OK ('+code.length+' chars)')}
-  catch(e){console.log('Script '+i+' ERROR: '+e.message)}
-});
